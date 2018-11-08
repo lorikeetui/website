19848184201c5b3cd42368b33b0a0ac2f24b7d64
陳詩華 <chenshihua@chenshihuade-MacBook-Air.local>
2018-11-08 14:04:15 +0000

    change avatars to dynamic

--- src/components/Home/Contributors.js
+++ src/components/Home/Contributors.js
@@ -1,48 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 import { SafeLink } from '@aragon/ui'
-import image1 from './assets/GithubContributions/1.png'
-import image2 from './assets/GithubContributions/2.png'
-import image3 from './assets/GithubContributions/3.png'
-import image4 from './assets/GithubContributions/4.png'
-import image5 from './assets/GithubContributions/5.png'
-import image6 from './assets/GithubContributions/6.png'
-import image7 from './assets/GithubContributions/7.png'
-import image8 from './assets/GithubContributions/8.png'
-import image9 from './assets/GithubContributions/9.png'
-import image10 from './assets/GithubContributions/10.png'
-import image11 from './assets/GithubContributions/11.png'
-import image12 from './assets/GithubContributions/12.png'
-import image13 from './assets/GithubContributions/13.png'
-import image14 from './assets/GithubContributions/14.png'
-import image15 from './assets/GithubContributions/15.png'
-import image16 from './assets/GithubContributions/16.png'
-import image17 from './assets/GithubContributions/17.png'
-import image18 from './assets/GithubContributions/18.png'
 
-const Projects = () => (
+class Projects extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      items: [],
+      loading: true
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.github.com/repos/aragon/aragon/contributors')
+    .then(res => res.json())
+    .then(json => {
+      console.log(json)
+      this.setState({
+        items: json,
+        loading: false
+      })
+    })
+    .catch(console.log('erro'))
+  };
+
+  render() {
+    return <Contributors data={this.state.items}/>
+  }
+}
+const Contributors = ({ data }) => (
   <Container
     href="https://github.com/lorikeetui/lorikeet/graphs/contributors"
     target="_blank"
   >
-    <Image src={image1} />
-    <Image src={image2} />
-    <Image src={image3} />
-    <Image src={image4} />
-    <Image src={image5} />
-    <Image src={image6} />
-    <Image src={image7} />
-    <Image src={image8} />
-    <Image src={image9} />
-    <Image src={image10} />
-    <Image src={image11} />
-    <Image src={image12} />
-    <Image src={image13} />
-    <Image src={image14} />
-    <Image src={image15} />
-    <Image src={image16} />
-    <Image src={image17} />
-    <Image src={image18} />
+  {data.map(item =>  <Image src={item.avatar_url} />)}
+
   </Container>
 )
 
@@ -56,6 +49,8 @@ const Container = styled(SafeLink)`
 `
 const Image = styled.img`
   margin: 5px 10px;
+  height: 52px;
+  border-radius: 50%;
 `
 
 export default Projects
